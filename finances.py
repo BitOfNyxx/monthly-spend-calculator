d76fd0a58fb5fbfc8477eff8a76dcf1b23ce57ea
nyxx <nyxx@bitofnyxx.me>
2026-08-18 12:01:49 +0100

    Add loop to input_monthly_income function

--- finances.py
+++ finances.py
@@ -3,13 +3,16 @@ def input_starting_balance() -> float:
         try:
             return round(float(input("Please input your starting balance: ")))
         except ValueError:
-             print("Value entered is not a valid number, please try again.")
+            print("Value entered is not a valid number, please try again.")
+
 
 def input_monthly_income() -> float:
+    while True:
         try:
             return float(input("Please input your monthly income post deductions: "))
         except ValueError:
             print("Value entered is not a valid number, please try again.")
-            
+
+
 def balance_post_income(starting_balance: float, monthly_income: float) -> float:
     return starting_balance + monthly_income
