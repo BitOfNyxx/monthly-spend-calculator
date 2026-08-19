b3b9ea7b0bc5ff75046879f391ae0c6bacb2986b
nyxx <nyxx@bitofnyxx.me>
2026-08-19 14:17:11 +0100

    MILESTONE 2: Add APR and interest functions
    Add apr_monthly_rate and add_interest_to_balance in preparation for milestone 3 (debts)

--- finances.py
+++ finances.py
@@ -11,3 +11,12 @@ def input_monthly_income() -> float:
 
 def balance_post_income(starting_balance: float, monthly_income: float) -> float:
     return starting_balance + monthly_income
+
+
+def apr_monthly_rate(apr_percent: float) -> float:
+    return (apr_percent / 100) / 12
+
+
+def add_interest_to_balance(balance: float, monthly_rate: float) -> float:
+    # Balance in this regards refers to the balance of the debt/loan/credit card etc
+    return balance * monthly_rate
